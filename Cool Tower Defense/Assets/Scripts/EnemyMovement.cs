58c5ba08fc9fb52b9ee29c9c55a04645b7e51c89
Lordor77 <lordor777@gmail.com>
2018-08-06 08:34:23 +0300

    2

---
+++ Cool Tower Defense/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    // Variables
+    public Transform home;
+
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(home.transform.position.x, home.transform.position.y, home.transform.position.z));
+    }
+}
