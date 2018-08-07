a3c29bbf4516fff7491920bcaee69d4efa39d369
Lordor77 <lordor777@gmail.com>
2018-08-07 21:10:00 +0300

    1

--- Cool Tower Defense/Assets/Scripts/EnemiesSpawner.cs
+++ Cool Tower Defense/Assets/Scripts/EnemiesSpawner.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class EnemiesSpawner : MonoBehaviour {
     // Variables
+    public GameObject enemy_01;
+    private float timer;
 
 
 	// Use this for initialization
 	void Start () {
-		
+        timer = 0f;//2f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Instantiate(enemy_01, transform.position, transform.rotation);
+            timer = 1f;//4f;
+        }
 	}
 }
