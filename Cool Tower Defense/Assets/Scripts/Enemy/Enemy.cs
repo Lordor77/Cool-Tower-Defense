d4eefe0e8ad6fcb6b85329f9aa747fabdae5ee5e
Noam <noam1sapir@gmail.com>
2018-08-13 01:20:21 +0300

    Enemy
    - First Enemy (+ Animation + paths)
    - Map improvements
    - Scripts improvements
    - GamePlay improvement

--- Cool Tower Defense/Assets/Scripts/Enemy/Enemy.cs
+++ Cool Tower Defense/Assets/Scripts/Enemy/Enemy.cs
@@ -37,26 +37,26 @@ public class Enemy : MonoBehaviour {
         }
            
 
-            //if (other.gameObject.CompareTag("Flag_01"))
-            //    target = GameObject.Find("Flag_02").GetComponent<Transform>();
+        if (other.gameObject.CompareTag("Flag_01"))
+            target = GameObject.Find("Flag_02").GetComponent<Transform>();
 
-            //else if (other.gameObject.CompareTag("Flag_02"))
-            //    target = GameObject.Find("Flag_03").GetComponent<Transform>();
+        else if (other.gameObject.CompareTag("Flag_02"))
+            target = GameObject.Find("Flag_03").GetComponent<Transform>();
 
-            //else if (other.gameObject.CompareTag("Flag_03"))
-            //    target = GameObject.Find("Flag_04").GetComponent<Transform>();
+        else if (other.gameObject.CompareTag("Flag_03"))
+            target = GameObject.Find("Flag_04").GetComponent<Transform>();
 
-            //else if (other.gameObject.CompareTag("Flag_04"))
-            //    target = GameObject.Find("Flag_05").GetComponent<Transform>();
+         else if (other.gameObject.CompareTag("Flag_04"))
+            target = GameObject.Find("Flag_05").GetComponent<Transform>();
 
-            //else if (other.gameObject.CompareTag("Flag_05"))
-            //    target = GameObject.Find("Flag_06").GetComponent<Transform>();
+         else if (other.gameObject.CompareTag("Flag_05"))
+            target = GameObject.Find("Flag_06").GetComponent<Transform>();
 
-            //else if (other.gameObject.CompareTag("Flag_06"))
-            //    target = GameObject.Find("Flag_07").GetComponent<Transform>();
+          else if (other.gameObject.CompareTag("Flag_06"))
+            target = GameObject.Find("Flag_07").GetComponent<Transform>();
 
-            //else if (other.gameObject.CompareTag("Flag_07"))
-            //    target = GameObject.Find("HomeCamp").GetComponent<Transform>();
+           else if (other.gameObject.CompareTag("Flag_07"))
+            target = GameObject.Find("HomeCamp").GetComponent<Transform>();
     }
 }
 
